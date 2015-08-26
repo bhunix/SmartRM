189f2d71ead8aefa580ab5e21235bf0b6ad54c62
mining <mining@miningtekiMac-3.local>
2015-08-26 10:08:15 +0800

    jet

---
+++ jet.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdint.h>
+typedef struct stu
+{
+    struct stu *a;
+    char b;
+}_stu;
+
+#define print(_buf, _n) do {\
+    int i; \
+    for (i=0;i<_n;i++) {\
+        printf("%d\n", _buf[i]);\
+    }\
+}while (0)\
+
+main(){
+    struct stu *stu = NULL;
+    stu = (struct stu *)malloc(sizeof(struct stu));
+    if (!stu)
+    {
+        printf ("nul\n");
+    }
+    free(stu);
+}
+
+compare(struct stu *stu)
+{
+    printf("%c\n", stu->b);
+}
+
+test(char *c)
+{
+    uint8_t a[2];
+    uint32_t s = 23;
+    ((unsigned char*)a)[0] = (unsigned char)(((s)) & 0xff);
+    ((unsigned char*)a)[1] = (unsigned char)((s>>8) & 0xff);
+
+    printf("%x\n", (s>>8) & 0xff);
+
+}
+
